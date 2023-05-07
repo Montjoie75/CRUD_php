a7bbe1ba705465ee9521a52e6a8b926ac5fc779e
meledandri <a_meledandri@hotmail.com>
2023-05-07 20:27:07 +0200

    sql

---
+++ application_202305072022.sql
@@ -0,0 +1,24 @@
+INSERT INTO application.application (name,partner,is_active,creation_date) VALUES
+	 ('0 Super Zic',0,0,'2001-08-05'),
+	 ('1 Super Zic',0,1,'2021-09-05'),
+	 ('2 Super Zic',1,0,'2017-06-06'),
+	 ('3 Super Zic',0,1,'2023-05-07'),
+	 ('4 Super Zic',1,1,'2020-04-07'),
+	 ('5 Super Zic',1,0,'2002-01-06'),
+	 ('6 Super Zic',0,1,'2011-03-02'),
+	 ('7 Super Zic',1,1,'2001-05-03'),
+	 ('8 Super Zic',0,0,'2012-09-02'),
+	 ('9 Super Zic',1,1,'2020-05-06');
+INSERT INTO application.application (name,partner,is_active,creation_date) VALUES
+	 ('10 Super Zic',0,1,'2022-01-06'),
+	 ('11 Super Zic',0,0,'2021-11-02'),
+	 ('12 Super Zic',0,0,'2008-10-05'),
+	 ('13 Super Zic',0,0,'2023-02-07'),
+	 ('14 Super Zic',1,0,'2005-09-01'),
+	 ('15 Super Zic',0,0,'2008-03-03'),
+	 ('16 Super Zic',0,1,'2005-08-07'),
+	 ('17 Super Zic',1,0,'2007-02-05'),
+	 ('18 Super Zic',1,0,'2014-07-03'),
+	 ('19 Super Zic',1,0,'2017-07-01');
+INSERT INTO application.application (name,partner,is_active,creation_date) VALUES
+	 ('20 Super Zic',0,1,'2004-10-07');
